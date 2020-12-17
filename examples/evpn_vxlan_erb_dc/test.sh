551c00b80fa334962f9109b01c447995e22cbbd8
aburston <aburston@juniper.net>
2020-12-17 03:52:40 -0800

    fixed test script

--- examples/evpn_vxlan_erb_dc/test.sh
+++ examples/evpn_vxlan_erb_dc/test.sh
@@ -16,14 +16,15 @@
 umask 0002
 
 # Creating of result files and making them R/W by everybody
-mkdir test/outputs
+mkdir -p test/outputs
+mkdir -p test/resource_files/tmp
 touch test/outputs/output.xml
 touch test/outputs/log.html
 touch test/outputs/report.html
 
 export PYTHONPATH=libraries
 
-(cd test && pybot -C ansi -L TRACE tests/)
+(cd test && robot -C ansi -L TRACE tests/)
 
 chmod -R 777 test/tests
 chmod -R 777 test/resource_files/tmp
