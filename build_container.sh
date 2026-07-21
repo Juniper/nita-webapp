6c81a0fba255faf7fb59fd0b63e0bf2236ca58f1
Till Belzer <55496588+tbelz@users.noreply.github.com>
2026-07-21 13:49:57 +0200

    ci: publish multi-architecture Webapp image

--- build_container.sh
+++ build_container.sh
@@ -4,18 +4,6 @@
 # SPDX-License-Identifier: Apache-2.0
 
 set -e
-ARCH=`uname -p`
-if [ $ARCH = aarch64 ] ; then # it is ARM64
-	# By some reason PyYAML does not work with 5.4 version so change it to 5.3
-        REQ_WEBAPP_BACKUP=/var/tmp/requirements.txt.$$
-        cp requirements.txt ${REQ_WEBAPP_BACKUP}
-        cat requirements.txt | sed 's/PyYAML==5.4/PyYAML==5.3/' > /var/tmp/requirements.tmp.$$
-        mv /var/tmp/requirements.tmp.$$ requirements.txt
-fi
-
-docker build -t juniper/nita-webapp:$(tr -d '\r\n[:space:]' < VERSION.txt) .
-
-if [ $ARCH = aarch64 ] ; then 
-        # restoring requirement.txt to the origial
-	 mv ${REQ_WEBAPP_BACKUP} requirements.txt
-fi
+docker build \
+  --tag "juniper/nita-webapp:$(tr -d '\r\n[:space:]' < VERSION.txt)" \
+  .
